3582563121a8212e557bacbb6eca956d2ea6f474
Quy Binh Nguyen (Binh) <qbinhhh164@gmail.com>
2024-10-20 22:30:36 +1100

    Update exception handling in insert_users.php

--- php/insert_users.php
+++ php/insert_users.php
@@ -3,10 +3,17 @@ include "dbconfig.php";
 
 $conn = getConn();
 
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
 $users_data_file = "users.csv";
 # process and insert data to 'Users' table 
 try {
-    # try to open the file
+    if (!is_readable($users_data_file)) {
+        throw new Exception("The file $users_data_file is not readable.");
+    }
+
     if (($file = fopen($users_data_file, "r")) === FALSE) {
         throw new Exception("Failed to open the file $users_data_file");
     }
@@ -22,20 +29,28 @@ try {
     $stmt->bind_param("ss", $firstname, $surname);
 
     while (($data = fgetcsv($file)) !== FALSE) {
-        $firstname = $data[0];
-        $surname = $data[1];
+        if (isset($data[0], $data[1]) && !empty(trim($data[0])) && !empty(trim($data[1]))) {
+            $firstname = $data[0];
+            $surname = $data[1];
 
-        if (!$stmt->execute()) {
-            throw new Exception("Failed to execute the statement: " . $stmt->error);
+            if (!$stmt->execute()) {
+                throw new Exception("Failed to execute the statement: " . $stmt->error);
+            }
         }
     }
 
     $stmt->close();
+    
     echo "Users inserted successfully.";
-    fclose($file);
 }catch (Exception $e) {
-    echo "Error " . $e->getMessage();
-}
+    echo "Error inserting users" . $e->getMessage();
+}finally {
+    if (isset($file) && is_resource($file)) {
+        fclose($file);
+    }
 
-$conn->close();
+    if ($conn) {
+        $conn->close();
+    }
+}
 ?>
